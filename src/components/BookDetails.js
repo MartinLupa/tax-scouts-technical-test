f9b77ad44bdd575f328e721541e7bc026b005902
MartinLupa <lupa_martin@hotmail.com>
2022-02-01 20:16:47 +0100

    Styling update on BookDetails, detected bug on currentBook state

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -3,7 +3,19 @@ import styled from "styled-components";
 import { GlobalContext } from "../App";
 
 const BookDetailsWrapper = styled.div`
+  display: flex;
+  align-items: center;
   margin: 40px;
+  display: flex;
+  max-width: 1200px;
+  text-align: justify;
+  img {
+    padding: 40px;
+    margin: 20px;
+    border-radius: 5px;
+    border: 1px solid black;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
 `;
 
 export const BookDetails = () => {
@@ -11,18 +23,43 @@ export const BookDetails = () => {
 
   console.log(currentBook);
   return (
-    <BookDetailsWrapper>
-      {/* <img src={currentBook[0]["@uri"]} alt="" /> */}
-      <h2>{currentBook[0]?.title}</h2>
-      <p>{currentBook[0]?.author}</p>
-      <p>{currentBook[0]?.flapcopy.replace(/(<([^>]+)>)/gi, "")}</p>
-      <p>{currentBook[0]?.formatname}</p>
-      <p>{currentBook[0]?.imprint}</p>
-      <p>{currentBook[0]?.isbn}</p>
-      <p>{currentBook[0]?.onsaledate}</p>
-      <p>{currentBook[0]?.pages}</p>
-      <p>{currentBook[0]?.priceusa}</p>
-      <p>{currentBook[0]?.formatname}</p>
-    </BookDetailsWrapper>
+    <div>
+      {currentBook.length === 1 ? (
+        <BookDetailsWrapper>
+          <img src={currentBook[0]["@uri"]} alt="" />
+          <div>
+            <h2>{currentBook[0]?.titleshort}</h2>
+            <p>
+              <b>Author:</b> {currentBook[0]?.author}
+            </p>
+            <p>
+              {currentBook[0]?.flapcopy
+                .substring(0, 1000)
+                .replace(/(<([^>]+)>)/gi, "") + "..."}
+            </p>
+            <p>
+              <b>Format:</b> {currentBook[0]?.formatname}
+            </p>
+            <p>
+              <b>Imprint:</b> {currentBook[0]?.imprint}
+            </p>
+            <p>
+              <b>ISBN:</b> {currentBook[0]?.isbn}
+            </p>
+            <p>
+              <b>Published:</b> {currentBook[0]?.onsaledate}
+            </p>
+            <p>
+              <b>Length:</b> {currentBook[0]?.pages} Pages
+            </p>
+            <p>
+              <b>Price:</b> EUR{currentBook[0]?.priceusa}
+            </p>
+          </div>
+        </BookDetailsWrapper>
+      ) : (
+        <h2>Search for a book</h2>
+      )}
+    </div>
   );
 };
